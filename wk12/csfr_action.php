e4648d7839f6965d5a7689d53c14d8f9ea1b9fd6
David Shi <shy1860@hotmail.com>
2020-03-31 15:27:42 -0400

    week 12

---
+++ wk12/csfr_action.php
@@ -0,0 +1,43 @@
+<?php
+
+//print_r($_REQUEST);
+if(!empty($_POST)){
+    $a =$_POST["username"];
+    $b=$_POST["password"];
+    echo ("Your user name is: ".$a." and password is: ".$b);}
+session_start();
+
+?>
+
+<html>
+<header>
+
+    <script>
+        function myFunc() {
+
+            if((document.forms["testform"]["username"].value!=="david")||(document.forms["testform"]["password"].value!=="password")){
+                document.getElementById("show").innerHTML = "Validation error";
+                return false;
+            }
+            return true;
+        }
+    </script>
+</header>
+<body>
+
+<form  action= "csfr.php"method="post" id="testform" onsubmit="return myFunc()">
+    Name: <input id="un" type="text" name="username"><br>
+    password: <input id="pw" type="text" name="password"><br>
+    <input type="submit"value="Submit">
+</form>
+<?php
+// Set session variables
+$_SESSION["confirmation"] = "session variables";
+
+?>
+
+
+
+<div id="show"></div>
+</body>
+</html>
